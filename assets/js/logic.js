3e2676d4146be263ec530dd3c653ce3132a3c203
Abel <egbeleabel@yahoo.com>
2022-12-06 22:18:17 +0000

    changes to logic and questions js files

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,12 @@
+// Targetting Various HTML Elements by ID
+var time = document.getElementById('time');
+var btn = document.getElementById('start');
+var questionWrap = document.getElementById('questions');
+var questionTitle = document.getElementById('question-title');
+var choiceOutput = document.getElementById('choices');
 
-var time = document.getElementById("time"); // Targeting the timer on the browser
-var btn = document.getElementById("start"); // Targeting the start button
+// Set Current Question Index To First Question
+var currentQuestionIndex = 0;
 
 // Create a function to set quiz time to decrement from 75s
 function timeLeft() {
@@ -18,7 +24,54 @@ function timeLeft() {
 
 };
 
+
+
+// Create a function to start the quiz
+function startQuiz() {
+    var currentQuestion = questions[currentQuestionIndex];  //Setting the current question to the first question in the array
+    questionTitle.innerText = currentQuestion.title;   //Setting the question title to the title of the current question
+    var choices = currentQuestion.choices;  //Setting the choices to the choices of the current question
+
+    choiceOutput.innerHTML = "";  //Setting the choice div to an empty string so choices can be added
+
+    /*function nextQuestion() {
+        for (var item of questions) {
+            currentQuestion = item;
+            item++;
+        }
+
+        return currentQuestion;
+*/
+    // For Loop to create a button for each choice in the current question title and choices
+    for (var i = 0; i < choices.length; i++) {
+        var choice = choices[i];  //Set choice to the current choice
+        var correctAnswer = currentQuestion.answer === choice;  //Compare choice with correct answer to question to see if it's right
+
+        //Creating a button for every choice in our questions array
+        choiceOutput.insertAdjacentHTML('beforeend', `
+        <button data-correct=${correctAnswer}>${choice}</button>
+        `);
+
+        //Once a choice is selected, the next question is loaded
+        choiceOutput.addEventListener('click', function() {
+                currentQuestionIndex++;    
+                currentQuestion = questions[currentQuestionIndex];
+                console.log(currentQuestion);          
+        });
+        
+            
+    } 
+
+    //Display the question and choices when start button is clicked
+    questionWrap.classList.remove('hide');
+}
+
+
+
 // Set the timer to start decrementing at the click of the start button by calling the timeLeft function in the event listener for the button
-startQuiz = btn.addEventListener ('click', function(){
+var countDown = btn.addEventListener ('click', function(){
+    document.getElementById('start-screen').classList.add('hide'); //This hides the start screen to display only the questions
     timeLeft();
+    startQuiz();
 });
+
---
+++ assets/js/questions.js
@@ -0,0 +1,22 @@
+var questions = [
+    {
+        title: "Java Script File Has An Extension Of_____",
+        choices: ['A .Java', 'B .js', 'C .XML', 'D .JavaScript'],
+        answer: 'B .js'
+    },
+    {
+        title: "IsNaN() Evalutes an Argument To Determine if Given Value_____",
+        choices: ['A Is Not A Null', 'B Is Not An Object', 'C Is Not A Number', 'D Is Not An Array'],
+        answer: 'C Is Not A Number'
+    },
+    {
+        title: "Which Of The Dialog Box Display a Message And a Data Entry Field?",
+        choices: ['A Prompt()', 'B Alert()', 'C Msg()', 'D Confirm()'],
+        answer: 'A Prompt()'  
+    },
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ['A Js', 'B JavaScript', 'C Scripting', 'D Script'],
+        answer: 'D Script'
+    },   
+]
