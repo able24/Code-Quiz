1da4f57beb2820ac0132a619b543d8bee8e936f4
Abel <egbeleabel@yahoo.com>
2022-12-20 00:56:28 +0000

    adds sound and notifications to correct and wrong answer choices

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,10 +4,17 @@ var btn = document.getElementById('start');
 var questionWrap = document.getElementById('questions');
 var questionTitle = document.getElementById('question-title');
 var choiceOutput = document.getElementById('choices');
+var correctAnswer = document.getElementById('correct');
+var incorrectAnswer = document.getElementById('wrong');
+var correctSound = new Audio("./assets/sfx/correct.wav");
+var incorrectSound = new Audio("./assets/sfx/incorrect.wav");
+var endScreen = document.getElementById('end-screen');
+var finalScore = document.getElementById('final-score');
+
 
 // Set Current Question Index To First Question
 var currentQuestionIndex = 0;
-var quizTime = 75;
+var quizTime = 60;
 // Create a function to set quiz time to decrement from 75s
 function timeLeft() {
 
@@ -53,20 +60,36 @@ function startQuiz() {
         //Display the question and choices when start button is clicked
         questionWrap.classList.remove('hide');
     } else {
-
         localStorage.setItem("Score", JSON.stringify(quizTime)); // Set score to time remaining after quiz
-    }
+        questionWrap.classList.add('hide'); // hides the last question
+        endScreen.classList.remove('hide'); // displays the page for test taker to enter his/her initials
+        finalScore.innerHTML = JSON.parse(localStorage.getItem("Score")); // displays the score for that test and asks for the test taker's initial
+
+    };
 }
 
-// Check if chosen answer is the correct answer. If correct answer, play correct answer sound. If wrong answer, play wrong answer sound and deduct 10s from quiz time
+
+// Check if chosen answer is the correct answer. If correct answer, display correct! and play correct answer sound. If wrong answer, display wrong!, play wrong answer sound and deduct 10s from quiz time
 choiceOutput.addEventListener('click', function (e) {
     e.preventDefault();
     if (e.target.innerText === questions[currentQuestionIndex].answer) {
-        console.log("correct")
-    } else { quizTime -= 10; };
+        correctSound.play(); // Plays sound to indicate correct answer
+        correctAnswer.classList.remove('hide'); // if answer is correct, show notification "correct!"
+        setTimeout(function () {
+            correctAnswer.classList.add('hide'); // Notification for correct answer stays for 0.5s and is hidden
+        }, 500);
 
 
-  // Go to the next question after an answer button is clicked
+    } else {
+        incorrectAnswer.classList.remove('hide'); // if answer is wrong, show noticiation "wrong!"
+        setTimeout(function () {
+            incorrectSound.play(); // Plays sound to indicate wrong answer
+            incorrectAnswer.classList.add('hide'); // Notification for wrong answer stays for 0.5s and then is hidden
+        }, 500);
+        quizTime -= 10; // 10s is deducted for wrong answers
+    };
+
+    // Go to the next question after an answer button is clicked
     currentQuestionIndex++;
     var currentQuestion = questions[currentQuestionIndex];
     //console.log(currentQuestion); 
