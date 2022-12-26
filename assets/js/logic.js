a47d9f1254ae9d845350ebef3a4af249ea691b55
Abel <egbeleabel@yahoo.com>
2022-12-26 11:21:33 +0000

    final edition of the code quiz

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,13 +4,13 @@ var btn = document.getElementById('start');
 var questionWrap = document.getElementById('questions');
 var questionTitle = document.getElementById('question-title');
 var choiceOutput = document.getElementById('choices');
-var correctAnswer = document.getElementById('correct');
-var incorrectAnswer = document.getElementById('wrong');
-var correctSound = new Audio("./assets/sfx/correct.wav");
-var incorrectSound = new Audio("./assets/sfx/incorrect.wav");
+var correctSound = new Audio('./assets/sfx/correct.wav');
+var incorrectSound = new Audio('./assets/sfx/incorrect.wav');
 var endScreen = document.getElementById('end-screen');
 var finalScore = document.getElementById('final-score');
-
+var initials = document.getElementById('initials');
+var submit = document.getElementById('submit');
+var feedback = document.getElementById('feedback');
 
 
 // Set Current Question Index To First Question
@@ -68,7 +68,13 @@ function startQuiz() {
         questionWrap.classList.add('hide'); // hides the last question
         endScreen.classList.remove('hide'); // displays the page for test taker to enter his/her initials
         finalScore.innerHTML = JSON.parse(localStorage.getItem("Score")); // displays the score for that test and asks for the test taker's initial
-
+        submit.addEventListener('click', function(e) {
+            e.preventDefault();
+            localStorage.setItem("Initials", JSON.stringify(initials.value));  // Stores the initial of the test take to localStorage upon the click of the submit button
+            endScreen.innerText = 'Thank You. Please click on the View Highscores on the top left-hand side of the page to see highscores';
+        
+            
+        });
     };
 }
 
@@ -78,17 +84,19 @@ choiceOutput.addEventListener('click', function (e) {
     e.preventDefault();
     if (e.target.innerText === questions[currentQuestionIndex].answer) {
         correctSound.play(); // Plays sound to indicate correct answer
-        correctAnswer.classList.remove('hide'); // if answer is correct, show notification "correct!"
+        feedback.classList.remove('hide');
+        feedback.innerText = 'correct!'; // if answer is correct, show notification "correct!"
         setTimeout(function () {
-            correctAnswer.classList.add('hide'); // Notification for correct answer stays for 0.5s and is hidden
+            feedback.classList.add('hide'); // Notification for correct answer stays for 0.5s and is hidden
         }, 500);
 
 
     } else {
-        incorrectAnswer.classList.remove('hide'); // if answer is wrong, show noticiation "wrong!"
+        feedback.classList.remove('hide'); 
+        feedback.innerText = 'wrong!'; // if answer is wrong, show noticiation "wrong!"
         setTimeout(function () {
             incorrectSound.play(); // Plays sound to indicate wrong answer
-            incorrectAnswer.classList.add('hide'); // Notification for wrong answer stays for 0.5s and then is hidden
+            feedback.classList.add('hide'); // Notification for wrong answer stays for 0.5s and then is hidden
         }, 500);
         quizTime -= 10; // 10s is deducted for wrong answers
     };
--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,12 +1,13 @@
 // Getting score from localStorage
 var highScore = JSON.parse(localStorage.getItem("Score"));
+var initial = JSON.parse(localStorage.getItem("Initials"));
 
 //Targetting the clear highscore button
 var clearHighScore = document.getElementById('clear');
 
 
 // Displaying the score when you click view highscore
-document.getElementById('highscores').innerHTML = highScore;
+document.getElementById('highscores').innerHTML = `${initial}: ${highScore}`;
 
 // Clearing out scores displayed once the clear highscore button is pressed
 clearHighScore.addEventListener ('click', function() {
